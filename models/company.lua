59771a7e96b118e32fde1a8ee27705a08d94ef35
Eduardo Henrique <ehfs@ic.ufal.br>
2022-11-24 23:56:10 -0300

    Make use of SQL database connection

--- models/company.lua
+++ models/company.lua
@@ -5,13 +5,12 @@ local printTable = function(table)
 end
 
 -- Meta class
-Company = { values = {}, name = '', cnpj = ''}
+Company = { name = '', cnpj = ''}
 
 function Company:new (o, values, name, cnpj)
 	o = o or {}
 	setmetatable(o, self)
 	self.__index = self
-	self.values = values
 	self.name = name
 	self.cnpj = cnpj
 	return o
@@ -20,10 +19,6 @@ end
 function Company:print ()
 	print("Company ")
 	print("Name: R$ ", self.name)
-	print("Values: ")
-	for k, v in pairs(table) do
-		print(k, '=', v)
-	end
 	print("CNPJ: ", self.cnpj)
 end
 
