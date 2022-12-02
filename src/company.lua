a1a4c7a760ebf34251202755cfd0527a874b4ccf
Eduardo Henrique <ehfs@ic.ufal.br>
2022-12-02 16:51:14 -0300

    Make use of database functions and services to perform request operations

--- src/company.lua
+++ src/company.lua
@@ -1,29 +1,37 @@
+local json = require('JSON')
+
 local db = require('db')
 db.connect()
 
 local company = {}
 
-company.registerCompany = function(o, data)
-	-- salvar no banco as informações da operadora
-	print('registerCompany')
+company.registerCompany = function(o, data, res)
+	local success = db.registerCompany(data)
+	if success then
+		res:addHeader('Content-Type', 'text/html'):statusCode(201, '201 Created!'):write('Operadora cadastrada!')
+	else res:addHeader('Content-Type', 'text/html'):statusCode(500, '500 Internal Error!'):write('Não pôde cadastrar a operadora!')
+	end
 end
 
-company.getCompanies = function(o)
-	-- retorna todas as operadoras cadastradas
-	print('getCompanies')
-	db.getAllCompanies()
+company.getCompanies = function(o, res)
+	local companies = db.getAllCompanies()
+	-- for idx, company in pairs(companies) do
+	-- 	for k, v in pairs(company) do
+	-- 		print(k, '=', v)
+	-- 	end
+	-- end
+	res:addHeader('Content-Type', 'application/json'):statusCode(200, "200 OK"):write(json:encode(companies))
 end
 
-company.getCompanyById = function(o, data)
-	print(data)
-	db.connect()
-	db.getCompanyById(data)
+company.getCompanyById = function(o, id, res)
+	res:addHeader('Content-Type', 'application/json'):statusCode(200, '200 OK!'):write(json:encode(db.getCompanyById(id['id'])))
 end
 
-company.removeCompany = function(o, data)
-	-- remove a operadora cadastrada
-	print('removeCompany')
-	db.removeCompany(data['id'])
+company.removeCompany = function(o, id, res)
+	local success = db.removeCompany(id['id'])
+	if success then res:addHeader('Content-Type', 'text/html'):statusCode(200, '200 Successful!'):write('Operadora removida!')
+	else res:addHeader('Content-Type', 'text/html'):statusCode(500, '500 Internal Error!'):write('Não foi possível realizar a alteração!')
+	end
 end
 
 return company
--- src/user.lua
+++ src/user.lua
@@ -3,17 +3,22 @@ db.connect()
 
 local user = {}
 
-user.getUserById = function(o, data)
-	-- retorna o objeto usuario caso exista
-	-- necessario comunicacao com o servico de conta
-	print('getUserById')
-	db.getAllUsers()
+user.getUserById = function(uid)
+	return db.getUserById(uid)
 end
 
-user.hasBalance = function(o, data)
-	-- checar se o usuario possui saldo suficiente para recarga
-	-- necessario comunicacao com o servico de conta
-	print('hasBalance')
+user.getUsers = function()
+	for idx, user in pairs(db.getAllUsers()) do
+		for k, v in pairs(user) do
+			print(k, '=', v)
+		end
+	end
+end
+
+user.hasBalance = function(uid, value)
+	local user = user.getUserById(uid)
+	if user.balance - value >= 0.0 then return true end
+	return false
 end
 
 return user
